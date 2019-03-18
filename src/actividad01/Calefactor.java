db9e4212edc1922f351c9ce3b299860635540005
Javier <ocagnen2@gmail.com>
2019-03-18 12:37:07 +0100

    Crea atributos, constructores, getters/setters, toString y métodos abrir/cerrar/fijarTemperatura

--- src/actividad01/Calefactor.java
+++ src/actividad01/Calefactor.java
@@ -11,4 +11,49 @@ package actividad01;
  */
 public class Calefactor {
     
+    private int temperatura;
+    private boolean estado;
+
+    public Calefactor(int temperatura, boolean estado) {
+        this.temperatura = temperatura;
+        this.estado = estado;
+    }
+    
+    public void encender(){
+        this.estado=true;
+    }
+    
+    public void apagar(){
+        this.estado=false;
+    }
+    
+    public void fijarTemperatura(int valor){
+        this.temperatura = valor;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Calefactor{" + "temperatura=" + temperatura + ", estado=" + estado + '}';
+    }
+    
+    
+    
+ 
+    
 }
