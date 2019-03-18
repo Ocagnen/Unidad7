25cf20becb393697d0d724be0570c87778903e97
Javier <ocagnen2@gmail.com>
2019-03-18 12:37:41 +0100

    Crea atributos, constructores, getters/setters y toString

--- src/actividad01/Casa.java
+++ src/actividad01/Casa.java
@@ -5,10 +5,57 @@
  */
 package actividad01;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author javier
  */
 public class Casa {
+    
+    private Puerta puerta;
+    private ArrayList<Ventana> ventana;    
+    private ArrayList<Calefactor> calefactor;
+
+    public Casa(Puerta puerta, ArrayList<Ventana> ventana, ArrayList<Calefactor> calefactor) {
+        this.puerta = puerta;
+        this.ventana = ventana;
+        this.calefactor = calefactor;
+    }
+
+    public Puerta getPuerta() {
+        return puerta;
+    }
+
+    public void setPuerta(Puerta puerta) {
+        this.puerta = puerta;
+    }
+
+    public ArrayList<Ventana> getVentana() {
+        return ventana;
+    }
+
+    public void setVentana(ArrayList<Ventana> ventana) {
+        this.ventana = ventana;
+    }
+
+    public ArrayList<Calefactor> getCalefactor() {
+        return calefactor;
+    }
+
+    public void setCalefactor(ArrayList<Calefactor> calefactor) {
+        this.calefactor = calefactor;
+    }
+
+    @Override
+    public String toString() {
+        return "Casa{" + "puerta=" + puerta + ", ventana=" + ventana + ", calefactor=" + calefactor + '}';
+    }
+    
+    
+
+   
+    
+    
     
 }
