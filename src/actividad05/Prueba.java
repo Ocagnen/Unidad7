f78a202e8804df1b9e2bc3e7c979ec11d700e74d
Javier <ocagnen2@gmail.com>
2019-03-19 14:39:36 +0100

    Prueba en el main de diferentes propiedades (transitiva, reflexiva,..)

--- src/actividad05/Prueba.java
+++ src/actividad05/Prueba.java
@@ -10,5 +10,45 @@ package actividad05;
  * @author javier
  */
 public class Prueba {
+    public static void main(String[] args) {
+        
+        Cliente clienteA = new Cliente();
+        Cliente clienteB = new Cliente("Javier","08955463A",35);
+        Cliente clienteC = new Cliente("Javier","08955463A",35);
+        Cliente clienteD = new Cliente("Javier","08955463A",35);
+        Cliente clienteE = new Cliente("Ana","35211458d",19);
+        
+        System.out.println(clienteA.hashCode());
+        System.out.println(clienteB.hashCode());
+        System.out.println(clienteC.hashCode());
+        System.out.println(clienteD.hashCode());
+        System.out.println(clienteE.hashCode());
+        System.out.println("");
+        
+        // Reflexiva
+        System.out.println("Reflexiva");
+        System.out.println(clienteB.equals(clienteC));
+        System.out.println(clienteA.equals(clienteB));
+        System.out.println("");
+        
+        // Simétrica
+        System.out.println("Simétrica");
+        System.out.println(clienteB.equals(clienteC));
+        System.out.println(clienteB.equals(clienteC));
+        System.out.println("");
+        
+        // Transitiva
+        System.out.println("Transitiva");
+        System.out.println(clienteB.equals(clienteC));
+        System.out.println(clienteC.equals(clienteD));
+        System.out.println(clienteB.equals(clienteD));
+        System.out.println("");
+        
+        //Nula
+        System.out.println("Nula");
+        System.out.println(clienteB.equals(null));
+        System.out.println("");
+    }
+    
     
 }
