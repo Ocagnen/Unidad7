de95a32c3630310fe04b6fed6498e155e08d4215
javier <ocagnen2@gmail.com>
2019-03-25 15:48:54 +0100

    Crea clase Cuenta (atributos,constructores,getters,..)

---
+++ src/actividad12/Cuenta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad12;
+
+/**
+ *
+ * @author Javier
+ */
+public class Cuenta {
+    
+    private long numeroCuenta;
+    private double saldo;
+    private Persona cliente;
+    
+}
