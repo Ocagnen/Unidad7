8618953386fb1803fd9fa581d01b419fbedd4593
Javier <ocagnen2@gmail.com>
2019-03-25 12:05:43 +0100

    Añado metodo propio y sobrescribo método de trabajdor

--- src/actividad07/Camarero.java
+++ src/actividad07/Camarero.java
@@ -9,6 +9,41 @@ package actividad07;
  *
  * @author javier
  */
-public class Camarero {
+public class Camarero extends Trabajador{
+    
+    private String rango;
+
+    public Camarero(String rango, String nombre, String apellido1, String NIF) {
+        super(nombre, apellido1, NIF);
+        this.rango = rango;
+    }
+    
+    public Camarero(){
+        this.rango = "encargado";
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+    
+    public void servirMesa(){
+        System.out.println("Sirviendo mesa");
+    }
+    
+    @Override
+    public void cotizar(){
+        System.out.println("Cotizando como camarero");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nCamarero{" + "rango=" + rango + '}';
+    }
+    
+    
     
 }
