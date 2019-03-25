b0b54c95ead7c5e136b2d4dcd57a4c863d084111
Javier <ocagnen2@gmail.com>
2019-03-25 12:39:58 +0100

    Crea main , crea dos objetos dado/moneda y prueba método lanzar

---
+++ src/actividad08/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad08;
+
+/**
+ *
+ * @author javier
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Azar dado1 = new Dado();
+        Azar moneda1 = new Moneda();
+
+        System.out.println(dado1.lanzar());
+        System.out.println(moneda1.lanzar());
+
+    }
+
+}
