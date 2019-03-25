6f021ae5cf9425de84c87851552161d5415456bc
javier <ocagnen2@gmail.com>
2019-03-25 15:52:15 +0100

    Añade métodos abstractos

--- src/actividad12/Cuenta.java
+++ src/actividad12/Cuenta.java
@@ -5,14 +5,61 @@
  */
 package actividad12;
 
+import java.util.Random;
+
 /**
  *
  * @author Javier
  */
-public class Cuenta {
+public abstract class Cuenta {
     
     private long numeroCuenta;
     private double saldo;
     private Persona cliente;
+
+    public Cuenta(Persona cliente) {  
+        Random alt = new Random();
+        this.cliente = cliente;
+        this.saldo = 0;        
+        this.numeroCuenta = alt.nextLong();
+    }
+
+    public Cuenta() {
+    }
+
+    public long getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(long numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Persona getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Persona cliente) {
+        this.cliente = cliente;
+    }
+    
+    public abstract void actualizarSaldo();
+    
+    public abstract void retirar(double saldoRetirado);
+
+    @Override
+    public String toString() {
+        return "Cuenta{" + "numeroCuenta=" + numeroCuenta + ", saldo=" + saldo + ", cliente=" + cliente + '}';
+    }
+    
+    
     
 }
