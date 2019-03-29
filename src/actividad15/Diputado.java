c40285c5d69cfecd389fc8f76837578a9727bc10
Javier <ocagnen2@gmail.com>
2019-03-29 12:24:32 +0100

    Crea clase Diputado que hereda de Legislador con atributos, consturctores, getters,..

---
+++ src/actividad15/Diputado.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad15;
+
+/**
+ *
+ * @author javier
+ */
+public class Diputado extends Legislador{
+    
+    private int numeroAsiento;
+
+    public Diputado(int numeroAsiento, String provinciaQueRepresenta, String partidoPolitico, String nombre, String apellidos) {
+        super(provinciaQueRepresenta, partidoPolitico, nombre, apellidos);
+        this.numeroAsiento = numeroAsiento;
+    }
+
+    public Diputado(){
+        
+    }
+    
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public void setNumeroAsiento(int numeroAsiento) {
+        this.numeroAsiento = numeroAsiento;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"\nDiputado" + "\tnumeroAsiento=" + numeroAsiento;
+    }
+
+    @Override
+    public String getCamaraEnQueTrabaja() {
+        return "Diputado";
+    }
+    
+    
+    
+}
